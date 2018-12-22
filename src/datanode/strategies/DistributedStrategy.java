66719867ea3c307ad13bc945beb6487666eb88bb
song4u <song4you@msn.cn>
2018-12-22 17:01:09 -0600

    import apache gossip repository

--- src/datanode/strategies/DistributedStrategy.java
+++ src/datanode/strategies/DistributedStrategy.java
@@ -1,29 +1,32 @@
 package datanode.strategies;
 
-import com.codahale.metrics.MetricRegistry;
 import commonmodels.DataNode;
-import org.apache.gossip.*;
+import org.apache.gossip.GossipSettings;
+import org.apache.gossip.LocalMember;
+import org.apache.gossip.Member;
+import org.apache.gossip.RemoteMember;
 import org.apache.gossip.event.GossipListener;
 import org.apache.gossip.event.GossipState;
+import org.apache.gossip.manager.GossipManager;
+import org.apache.gossip.manager.GossipManagerBuilder;
 import util.URIHelper;
 
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
 public class DistributedStrategy extends MembershipStrategy implements GossipListener {
 
-    private GossipService gossipService;
+    private GossipManager gossipService;
 
     public DistributedStrategy(DataNode dataNode) {
         super(dataNode);
     }
 
     @Override
-    public void gossipEvent(GossipMember gossipMember, GossipState gossipState) {
+    public void gossipEvent(Member gossipMember, GossipState gossipState) {
         switch (gossipState) {
             case UP:
                 dataNode.onNodeUp(gossipMember.getClusterName(), gossipMember.getUri().getHost(), gossipMember.getUri().getPort());
@@ -38,7 +41,7 @@ public class DistributedStrategy extends MembershipStrategy implements GossipLis
     public void onNodeStarted() throws InterruptedException, UnknownHostException, URISyntaxException {
         super.onNodeStarted();
         initGossipManager(dataNode);
-        gossipService.start();
+        gossipService.init();
     }
 
     @Override
@@ -51,39 +54,41 @@ public class DistributedStrategy extends MembershipStrategy implements GossipLis
         return printLiveMembers() + printDeadMembers();
     }
 
-    private void initGossipManager(DataNode dataNode) throws URISyntaxException, UnknownHostException, InterruptedException {
+    private void initGossipManager(DataNode dataNode) throws URISyntaxException {
         GossipSettings settings = new GossipSettings();
         settings.setWindowSize(1000);
         settings.setGossipInterval(1000);
-        List<GossipMember> startupMembers = new ArrayList<>();
+        List<Member> startupMembers = new ArrayList<>();
 
         for (String seed : dataNode.getSeeds()) {
             URI uri = URIHelper.getGossipURI(seed);
-            startupMembers.add(new RemoteGossipMember(
+            startupMembers.add(new RemoteMember(
                     dataNode.getClusterName(),
                     uri,
                     uri.getHost() + "." + uri.getPort()));
         }
 
-        this.gossipService = new GossipService(
-                dataNode.getClusterName(),
-                URIHelper.getGossipURI(dataNode.getAddress()),
-                dataNode.getGossipId(), new HashMap<>(),
-                startupMembers, settings,
-                this, new MetricRegistry());
+        this.gossipService = GossipManagerBuilder.newBuilder()
+                                .cluster(dataNode.getClusterName())
+                                .uri(URIHelper.getGossipURI(dataNode.getAddress()))
+                                .id(dataNode.getGossipId())
+                                .gossipMembers(startupMembers)
+                                .gossipSettings(settings)
+                                .listener(this)
+                                .build();
     }
 
     private String printLiveMembers() {
-        List<LocalGossipMember> members = gossipService.getGossipManager().getLiveMembers();
+        List<LocalMember> members = gossipService.getLiveMembers();
         return getMemberStatus("Live: ", members);
     }
 
     private String printDeadMembers() {
-        List<LocalGossipMember> members = gossipService.getGossipManager().getDeadMembers();
+        List<LocalMember> members = gossipService.getDeadMembers();
         return getMemberStatus("Dead: ", members);
     }
 
-    private String getMemberStatus(String title, List<LocalGossipMember> members) {
+    private String getMemberStatus(String title, List<LocalMember> members) {
         StringBuilder result = new StringBuilder();
         result.append(title).append('\n');
 
@@ -91,7 +96,7 @@ public class DistributedStrategy extends MembershipStrategy implements GossipLis
             result.append("None\n");
         }
         else {
-            for (LocalGossipMember member : members)
+            for (LocalMember member : members)
                 result.append("Node: ").append(member.getId()).append(" ")
                         .append("Heartbeat: ").append(member.getHeartbeat()).append('\n');
         }
