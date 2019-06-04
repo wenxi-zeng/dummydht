d05a0b2a2c67edd024d5683bb51de2200184e066
song4u <song4you@msn.cn>
2019-06-04 15:59:20 -0500

    decentralized load change handler for ceph

--- src/ceph/CephDecentLoadChangeHandler.java
+++ src/ceph/CephDecentLoadChangeHandler.java
@@ -1,66 +1,49 @@
 package ceph;
 
-import commands.CephCommand;
 import commonmodels.Clusterable;
-import commonmodels.LoadChangeHandler;
 import commonmodels.PhysicalNode;
 import commonmodels.transport.Request;
 import loadmanagement.LoadInfo;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
-public class CephLoadChangeHandler implements LoadChangeHandler {
-
-    protected final ClusterMap map;
-
-    public CephLoadChangeHandler(ClusterMap map) {
-        this.map = map;
+public class CephDecentLoadChangeHandler extends CephLoadChangeHandler {
+    public CephDecentLoadChangeHandler(ClusterMap map) {
+        super(map);
     }
 
     @Override
-    public List<Request> generateRequestBasedOnLoad(List<LoadInfo> globalLoad, LoadInfo loadInfo, long lowerBound, long upperBound) {
+    public List<Request> generateRequestBasedOnLoad(List<LoadInfo> loadInfoList, LoadInfo loadInfo, long lowerBound, long upperBound) {
+        if (loadInfoList == null || loadInfoList.size() < 1) return null;
+
         PhysicalNode node = new PhysicalNode(loadInfo.getNodeId());
         node = map.getPhysicalNodeMap().get(node.getId());
         Clusterable parent = map.findParentOf(node);
 
-        Map<String, LoadInfo> map = globalLoad.stream().collect(
-                Collectors.toMap(LoadInfo::getNodeId, info -> info));
         float halfWeight = 0;
-        int numberOfLightNodes = 0;
         for (int i = 0; i < parent.getSubClusters().length; i++) {
             Clusterable child = parent.getSubClusters()[i];
             if (child == null) continue;
             if (child instanceof PhysicalNode) {
                 PhysicalNode pchild = (PhysicalNode) child;
-                LoadInfo info = map.get(pchild.getFullAddress());
 
                 if (pchild.getId().equals(node.getId())) {
                     halfWeight = pchild.getWeight() / 2;
                 }
-                else if(info.getLoad() < lowerBound) {
-                    numberOfLightNodes++;
-                }
             }
         }
 
-        if (numberOfLightNodes < 1) return null;
-
-        float distributeWeight = halfWeight / numberOfLightNodes;
         for (int i = 0; i < parent.getSubClusters().length; i++) {
             Clusterable child = parent.getSubClusters()[i];
             if (child == null) continue;
             if (child instanceof PhysicalNode) {
                 PhysicalNode pchild = (PhysicalNode) child;
-                LoadInfo info = map.get(pchild.getFullAddress());
 
                 if (pchild.getId().equals(node.getId())) {
                     pchild.setWeight(halfWeight);
                 }
-                else if(info.getLoad() < lowerBound) {
-                    pchild.setWeight(pchild.getWeight() + distributeWeight);
+                else if(loadInfoList.get(0).getNodeId().equals(node.getId())) {
+                    pchild.setWeight(pchild.getWeight() + halfWeight);
                 }
             }
         }
@@ -69,16 +52,5 @@ public class CephLoadChangeHandler implements LoadChangeHandler {
         // since every node has to have a update request.
         // requests are generated in the optimize method.
         return null;
-    }
-
-    @Override
-    public void optimize(List<Request> requests) {
-        requests.add(new Request().withHeader(CephCommand.PROPAGATE.name()));
-    }
-
-    @Override
-    public long computeTargetLoad(List<LoadInfo> loadInfoList, LoadInfo loadInfo, long lowerBound, long upperBound) {
-        // stub
-        return 0;
     }
 }
